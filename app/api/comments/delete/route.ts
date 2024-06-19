f1d70673d27c9435f9bbaa2386fa517c3940c395
Cruizzer <archcruizer@gmail.com>
2024-06-19 12:27:56 +0100

    Added delete for ease of use

---
+++ app/api/comments/delete/route.ts
@@ -0,0 +1,20 @@
+import dbConnect from "@/lib/mongo";
+import Comment from "@/app/models/Comment";
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request) {
+    try {
+        await dbConnect();
+        const { searchParams } = new URL(req.url);
+        const param: string | null = searchParams.get("page");
+        const result = await Comment.deleteMany({page: param});
+
+        if (result.deletedCount > 0) {
+            return NextResponse.json({message: `${result.deletedCount} Comments deleted successfully`});
+        }
+
+        return NextResponse.json({message: "No comments found for this page"});
+    } catch (err: any) {
+        return NextResponse.json({error : err.message})
+    }
+}
